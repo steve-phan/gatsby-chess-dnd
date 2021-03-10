58063bb7165596db5376f11bf0fb520e815bbfb1
vUnAmp <phanhaingoc@gmail.com>
2021-03-10 18:55:08 +0100

    render UI

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+
+import styled from 'styled-components'
 
 import firebase from '../../firebase/utils.firebase'
+
+const Answer = styled.div``
+
 const Game = () => {
-  useState(() => {
+  const [questions, setQuestions] = useState([])
+  const [curQuestion, setCurQuestion] = useState()
+  const questionRef = useRef()
+  const [index, setIndex] = useState(false)
+
+  useEffect(() => {
     firebase
       .firestore()
       .collection('quiz')
@@ -10,21 +20,84 @@ const Game = () => {
       .then(querySnapshot => {
         let data = []
         querySnapshot.forEach(doc => {
-          // doc.data() is never undefined for query doc snapshots
-          //   console.log(doc.id, ' => ', doc.data())
-          data = [...data, doc.data()]
+          data = [
+            ...data,
+            {
+              [doc.id]: doc.data(),
+            },
+          ]
         })
         return data
       })
-      .then(result => console.log(result))
+      .then(result => {
+        const klasse2 = result.reduce((acc, cur) => {
+          if (cur.hasOwnProperty('klasse2')) {
+            // console.log(cur['klasse2'])
+            return {
+              ...acc,
+              ...cur['klasse2'],
+            }
+          } else {
+            return {
+              ...acc,
+            }
+          }
+        }, {})
+        setQuestions(klasse2)
+      })
       .catch(error => {
         console.log('Error getting documents: ', error)
       })
   }, [])
+  const handleGetStart = () => {
+    // if (!isShowQuestion) {
+    //   setIsShowQuestion(true)
+    // } else {
 
+    const rdNumber = Math.floor(Math.random() * questions.math.length)
+    questionRef.current = rdNumber
+    // console.log(questions.math[questionRef.current])
+
+    setCurQuestion(questions.math[questionRef.current])
+    // Primitivie variable type !
+    questions.math.splice(questionRef.current, 1)
+    setQuestions({
+      ...questions,
+    })
+
+    // }
+  }
+
+  console.log(curQuestion)
   return (
     <div>
       <h1>Hello</h1>
+
+      {curQuestion && (
+        <div className="game-board">
+          <h3>{curQuestion?.question}</h3>
+          <div>
+            {curQuestion.answers.map((answer, index) => {
+              return (
+                <Answer
+                  correct_answer={index}
+                  onClick={e => {
+                    if (curQuestion.correct_answer === index) {
+                      e.target.style.background = 'red'
+                    }
+                    setIndex(index)
+                  }}
+                  key={index}
+                >
+                  {answer}
+                </Answer>
+              )
+            })}
+          </div>
+        </div>
+      )}
+
+      <button onClick={handleGetStart}>get Start</button>
     </div>
   )
 }
